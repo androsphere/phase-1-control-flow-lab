1000aa82cee60959ed4747eb8bec7fbafc0070db
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-11 22:34:08 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  let message;
+  let distance = value;
+  if (distance <= 400){
+    message = "This one is on me!";
+  } else if (distance > 400 && distance < 2000){
+    message = "That will be twenty bucks.";
+  } else if (distance >= 2000 && distance <= 2500){
+    message = "I will gladly take your thirty bucks.";
+  } else{
+    message = "No can do.";
+  }
+  return message;
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(value){
   // Write your code here!
+  let city = value;
+  let message = city === "NYC" ? "Ok, sounds good.":"No go.";
+  return message ;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(value){
   // Write your code here!
+  let tip = value;
+  let message;
+  switch (tip){
+    case 'generous':
+      message = "Thank you so much.";
+      break;
+    case 'not as generous':
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye.";
+      break;
+  }
+  return message;
+
 }
